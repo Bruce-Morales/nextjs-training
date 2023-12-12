9f4a0b56469632a3f40934a05275aab0a254f393
Bruce Morales <Bruce.s.morales@gmail.com>
2023-12-12 12:04:48 -0800

    add images

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 export default function Home() {
   return (
     <main className="pt-32 px-5 text-center">
+       <div id="factContainer" className="color-gray"></div>
     <div className="max-w-[750px] mx-auto">
       <h1 className="text-4xl font-bold">The Trouble Twins</h1>
       <p className="leading-8">
