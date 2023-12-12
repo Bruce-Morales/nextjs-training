b190f4a2c5dda69160964b297c8283fd5b06aaa1
Bruce Morales <Bruce.s.morales@gmail.com>
2023-12-12 12:25:20 -0800

    remove old

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 
+
 export default function Home() {
   return (
     <main className="pt-32 px-5 text-center">
-       <div id="factContainer" className="m-0 p-0 bg-blue-500"></div>
+       
     <div className="max-w-[750px] mx-auto">
       <h1 className="text-4xl font-bold bg-orange-500 p-5">The Trouble Twins</h1>
       <p className="leading-8 bg-slate-200 p-2">
