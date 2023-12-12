cae11a5f8feae8f85458c35dbf52667fb3797a37
Bruce Morales <Bruce.s.morales@gmail.com>
2023-12-11 17:11:21 -0800

    added first lines

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="text-center pt-32">
+      <h1 className="text-4xl font-bold">The Trouble Twins</h1>
     </main>
   )
 }
