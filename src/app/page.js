1e5426ce0e19366199c73fb9cab88e835ee1b54c
Bruce Morales <Bruce.s.morales@gmail.com>
2023-12-11 17:22:38 -0800

    adjusted text location

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 
 export default function Home() {
   return (
-    <main className="text-center pt-32 px-5">
+    <main className="pt-32 px-5 text-center">
+    <div className="max-w-[750px] mx-auto">
       <h1 className="text-4xl font-bold">The Trouble Twins</h1>
-      <p className="max-w-[750px] mx-auto-leading-8">Maximus and Augustus are two brothers of the yorkie terrier species. Their predecessor in our house, Blaze, was also a yorkie terrier, but he was a lot smaller than these two are.</p>
-    </main>
+      <p className="leading-8">
+        Maximus and Augustus are two brothers of the yorkie terrier species. Their predecessor in our house, Blaze, was also a yorkie terrier, but he was a lot smaller than these two are.
+      </p>
+    </div>
+  </main>
   )
 }
