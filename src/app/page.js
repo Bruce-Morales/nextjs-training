62a4d9b58940128631bb849b9517a22adbfbf393
Bruce Morales <Bruce.s.morales@gmail.com>
2023-12-12 12:18:41 -0800

    more color

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
        <div id="factContainer" className="m-0 p-0 bg-blue-500"></div>
     <div className="max-w-[750px] mx-auto">
       <h1 className="text-4xl font-bold bg-orange-500 p-5">The Trouble Twins</h1>
-      <p className="leading-8">
+      <p className="leading-8 bg-slate-200 p-2">
         Maximus and Augustus are two brothers of the yorkie terrier species. Their predecessor in our house, Blaze, was also a yorkie terrier, but he was a lot smaller than these two are.
       </p>
     </div>
